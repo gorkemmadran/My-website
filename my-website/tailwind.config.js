90c691c1292ef0798fccd6477c80f389da11fd84
Gorkem Madran <gorkemmadran@hotmail.com>
2024-08-10 16:17:41 +0300

    Tailwind.config.js Güncellendi

--- my-website/tailwind.config.js
+++ my-website/tailwind.config.js
@@ -11,12 +11,12 @@ module.exports = {
         'opensans': ['Open Sans', 'sans-serif'],
       },
       screens: {
-        'xs': '400px',
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px',
+        'xs': {'min': '320px', 'max': '639px'}, // Çok küçük ekranlar (telefonlar)
+        'sm': {'min': '640px', 'max': '767px'}, // Küçük ekranlar (büyük telefonlar)
+        'md': {'min': '768px', 'max': '1023px'}, // Orta ekranlar (tabletler)
+        'lg': {'min': '1024px', 'max': '1279px'}, // Büyük ekranlar (küçük dizüstü bilgisayarlar)
+        'xl': {'min': '1280px', 'max': '1535px'}, // Çok büyük ekranlar (büyük dizüstü bilgisayarlar)
+        '2xl': {'min': '1536px'}, // Ekstra büyük ekranlar (masaüstü ve yukarısı)
       },
     },
   },
